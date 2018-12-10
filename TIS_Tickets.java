7424dab4b15d0c460c55cc9e84856330545298a8
DenizArasK <denizarask@hotmail.com>
2018-12-10 12:31:53 +0100

    HashMap

--- TIS_Tickets.java
+++ TIS_Tickets.java
@@ -1,8 +1,24 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TIS_Tickets {
 
-  public static ArrayList<String> child = new ArrayList<String>();
+  HashMap<String,Double> tickets = new HashMap<String,Double>();
+  tickets.put("Child",10.00);
+  tickets.put("Young adult",15.50);
+  tickets.put("Adult",22.50);
+  tickets.put("Senior",15.00);
+  tickets.put("Family",40.50);
+
+}
+
+
+
+
+
+
+/*  public static ArrayList<String> child = new ArrayList<String>();
   public static ArrayList<String> youngAdult = new ArrayList<String>();
   public static ArrayList<String> adult = new ArrayList<String>();
   public static ArrayList<String> adultSeniors = new ArrayList<String>();
@@ -21,6 +37,4 @@ public class TIS_Tickets {
 
   senior.add("Fam: ");
 
-  }
-
-}
+} */
