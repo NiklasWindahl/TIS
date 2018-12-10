e53970c2abe3809b97e8497711b5ab31880548ac
Niklas Windahl <nikles90@hotmail.com>
2018-12-10 11:22:01 +0100

    Added method for print

--- TIS_Main.java
+++ TIS_Main.java
@@ -1,15 +1,27 @@
 import java.util.*;
-  // ska hanter all output och input
-public class TIS_Main{
-  // Classvariables
-  // method to call tickets.java
-  // method to call transactions.java
-  // Welcome System.out.print();
+import java.util.Scanner;
+    // ska hanter all output och input
+  public class TIS_Main{
+    // Classvariables
+    // method to call tickets.java
+    // method to call transactions.java
+    // Welcome System.out.print();
+    private void printTickets() {
+      // Call to tickets.java
+      System.out.printl('\n' + );
+    }
+
+    private void
 
 
-public static void main(String[] args){
-// print method
-//
 
-}
-}
+
+
+  public static void main(String[] args){
+    TTK_Main tis_main = new TIS_Main();
+    Scanner scan = new Scanner(System.in);
+    // print method
+    // run Methods
+
+  }
+  }
