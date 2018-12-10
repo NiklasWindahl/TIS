02a2935628cee4ea5088894ac03a0517603103f3
Jeppe <j3ppes@gmail.com>
2018-12-10 11:22:32 +0100

    lagt till funktionalitet i createPayment

--- TIS_Transaction.java
+++ TIS_Transaction.java
@@ -1,10 +1,32 @@
 public class TIS_Transaction{
 
 TIS_Tickets Ticket = new TIS_Tickets();
+MHS_Bank Bank = new MHS_Bank();
 
+Scanner scan = new Scanner(System.in);
+
+String transaction = "-1";
+double cashAmount = 0;
 // MHS get bankaccount
 // MHS get bank
 
+public String createPayment(){
+  System.out.println("Please enter bankaccount number: ");
+  String bankaccount = scan.next();
+  System.out.println("Please choose ticket: ");
+  //
+  /* for (int i = 0; i < Bank.accounts.size(); i++) {
+    if (bankaccount.equals(Bank.accounts.get(i))) {
+      bankaccount = -1;
+    }
+  } */
+  return bankaccount;
+  return cashAmount;
+}
+
+checkPayment(){
+  //Bank.check();
+}
 
 public static void main(String[] args) {
 
