137696d9b91a9499907327e44c21be432266e4d3
AldoRiz <aldoruiz342@hotmail.com>
2018-12-10 10:55:35 +0100

    love

--- TIS_Tickets.java
+++ TIS_Tickets.java
@@ -1,4 +1,11 @@
+import java.util.ArrayList;
+
 public class TIS_Tickets {
 
-
+  public static ArrayList<String> child = new ArrayList<String>();
+  public static ArrayList<String> youngAdult = new ArrayList<String>();
+  public static ArrayList<String> adult = new ArrayList<String>();
+  public static ArrayList<String> adultSeniors = new ArrayList<String>();
+  public static ArrayList<String> seionr= new ArrayList<String>();
+  public static ArrayList<String> familyTicket = new ArrayList<String>();
 }
