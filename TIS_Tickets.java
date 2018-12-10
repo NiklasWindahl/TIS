40d82a9163e4e5fe542cf28f580dee4b5ce6fcea
DenizArasK <denizarask@hotmail.com>
2018-12-10 11:23:26 +0100

    createBuyer

--- TIS_Tickets.java
+++ TIS_Tickets.java
@@ -8,4 +8,19 @@ public class TIS_Tickets {
   public static ArrayList<String> adultSeniors = new ArrayList<String>();
   public static ArrayList<String> seionr= new ArrayList<String>();
   public static ArrayList<String> familyTicket = new ArrayList<String>();
+
+  public void createBuyer() {
+
+  child.add("Child: ");
+
+  youngAdult.add("Young adult: ");
+
+  adult.add("Adult: ");
+
+  senior.add("Senior: ");
+
+  senior.add("Fam: ");
+
+  }
+
 }
