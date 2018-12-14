3157bdcea9cfe7a79c1405b87ae56db5043001bb
Jeppe <j3ppes@gmail.com>
2018-12-14 09:11:46 +0100

    ännu mer snygga ändringar

--- TIS_Transaction.java
+++ TIS_Transaction.java
@@ -4,14 +4,12 @@ public class TIS_Transaction{
 
   static MHS_Main bank = new MHS_Main();
 
-  // TIS_Tickets ticket = new TIS_Tickets();
-  // Scanner scan = new Scanner(System.in);
-  // TIS_Main main = new TIS_Main();
-
-
   String transaction = "-1";
   double cashAmount = 0;
 
+  // TIS_Tickets ticket = new TIS_Tickets();
+  // Scanner scan = new Scanner(System.in);
+  // TIS_Main main = new TIS_Main();
 
 
 public static Payment createPayment(String bankaccount, double cashAmount){
